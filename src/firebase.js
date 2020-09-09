d5264c9c2eb16a324c6351bdb55e6bb5c2b916f6
Ayodeji Adebola <Dopeman827@gmail.com>
2020-09-09 13:18:49 +0100

    firebase.js config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAkvt_EM5pviTzjCBcdzejmjiIaWPIBoKE",
+    authDomain: "clone-1e5df.firebaseapp.com",
+    databaseURL: "https://clone-1e5df.firebaseio.com",
+    projectId: "clone-1e5df",
+    storageBucket: "clone-1e5df.appspot.com",
+    messagingSenderId: "622584858287",
+    appId: "1:622584858287:web:b27678ae0f66627605365a",
+    measurementId: "G-39EXGBPBT6"
+  };
